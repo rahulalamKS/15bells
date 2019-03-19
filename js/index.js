c8d6b7c92f73d23e093e99743eedbdccd9ac8a9f
KSrahul <rahul@ampersanddesign.io>
2019-03-19 18:45:09 +0530

    optimize css and js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Check document is loaded
 document.onreadystatechange = function () {
     setTimeout(function () {
         if (document.readyState == "complete") {
@@ -6,12 +7,12 @@ document.onreadystatechange = function () {
             document.querySelector(".loaderContainer").className = "loaderContainer";
         }
     }, 1300)
-
 }
-window.onload = function () {
+// Check document is loaded (End)
 
+window.onload = function () {
+    // for onload animation
     const slideClass = document.querySelectorAll(".slide_lines");
-    const aniClass = document.querySelectorAll(".slide_ani");
 
     setTimeout(function () {
         slideClass.forEach(function (slides) {
@@ -19,24 +20,14 @@ window.onload = function () {
         })
     }, 1300)
 
-    aniClass.forEach(function (targetAni) {
-        targetAni.addEventListener("click", function () {
-            slideClass.forEach(function (removeClass) {
-                removeClass.classList.remove("animation");
-            })
-            setTimeout(function () {
-                slideClass.forEach(function (addClass) {
-                    addClass.classList.toggle("animation");
-                })
-            }, 0)
-        })
-    });
-
     setTimeout(function () {
         document.querySelector(".header").classList.add("ani_header");
         document.querySelector(".banner_cont").classList.add("banner_ani");
-        document.querySelector(".steps_cont").classList.add("steps_cont_show");
+        if (document.querySelector(".steps_cont")) {
+            document.querySelector(".steps_cont").classList.add("steps_cont_show");
+        }
     }, 2000)
+    // for onload animation (End)
 
     // for header scroll
     window.onscroll = function () {
@@ -52,7 +43,6 @@ window.onload = function () {
             document.querySelector(".logo_color").classList.remove("logo_color_change");
             $(".sign_login").removeClass("color_login_signup");
         }
-        // console.log(scroll);
     }
     // for header scroll (end)
 
@@ -72,36 +62,54 @@ window.onload = function () {
     }
     // onlick hamburger menu close (end)
 
-
+    // onclick open city list modal
     document.querySelector(".current_city_container").onclick = function () {
         document.querySelector(".city_internal").classList.add("show_city_overlay")
         document.querySelector(".overlay").classList.toggle("overlay_show")
         document.body.style.overflowY = "hidden";
     }
+    // onclick open city list modal (End)
 
+    // onclick close city list modal
     document.querySelector(".cross_city_overlay").onclick = function () {
         document.querySelector(".city_internal").classList.remove("show_city_overlay")
         document.querySelector(".overlay").classList.remove("overlay_show")
         document.body.style.overflowY = "scroll";
     }
+    // onclick close city list modal (End)
 
+    // when click outside close city list modal if it's open
     document.querySelector(".overlay").onclick = function () {
         document.querySelector(".city_internal").classList.remove("show_city_overlay")
         document.querySelector(".overlay").classList.remove("overlay_show")
         document.body.style.overflowY = "scroll";
     }
+    // when click outside close city list modal if it's open (End)
 
-    document.querySelector(".back_btn").onclick = function () {
-        window.history.back();
+    // onclick go to previous page
+    if (document.querySelector(".back_btn")) {
+        document.querySelector(".back_btn").onclick = function () {
+            window.history.back();
+        }
     }
+    // onclick go to previous page (End)
 
-    if ((window.location == "https://rahulalamks.github.io/15bells/checkbox.html") || (window.location == "https://rahulalamks.github.io/15bells/radion.html")) {
-        window.onresize = function () {
-            if (window.outerWidth < 751) {
-                document.querySelector(".header_menus .black_menus").classList.remove("black_menus");
-            } else {
-                document.querySelector(".header_menus .black_menus").classList.add("black_menus");
-            }
+
+    // if ((window.location == "https://rahulalamks.github.io/15bells/checkbox.html") || (window.location == "https://rahulalamks.github.io/15bells/radion.html") || (window.location == "https://rahulalamks.github.io/15bells/form.html")) {
+    //     window.onresize = function () {
+    //         if (window.outerWidth < 751) {
+    //             document.querySelector(".header_menus .black_menus").classList.remove("black_menus");
+    //         } else {
+    //             document.querySelector(".header_menus .black_menus").classList.add("black_menus");
+    //         }
+    //     }
+    // }
+
+    if (document.querySelector(".black_menus")) {
+        if (window.innerWidth < 751) {
+            document.querySelector(".header_menus .black_menus").classList.remove("black_menus");
+        } else {
+            document.querySelector(".header_menus .black_menus").classList.add("black_menus");
         }
     }
 }
